9a1417e128675d10292aeb81af8121e0032f72dc
alvaro-chiri <alvaro.chirinos@yahoo.com>
2022-07-16 15:19:28 +0000

    save

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
+
 export const Home = () => (
 	<div className="text-center mt-5">
 		<h1>Hello Rigo!</h1>
